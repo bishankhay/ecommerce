19bc2be9d91a2ffe86b4fa50a8df48b2f0a7c164
bishankhay <bishankhay@gmail.com>
2024-05-25 23:19:19 +0545

    loging and registration page

--- ecommerce/src/app/login/page.js
+++ ecommerce/src/app/login/page.js
@@ -1,9 +1,45 @@
-import React from 'react'
+'use client'
+// Render Prop
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const login= () => (
+  <div>
+    <h1>Welcome to Megamart! Please login</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          
+          <button type="submit" disabled={isSubmitting}>
+            LOGIN
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
 
-export default page
+export default login;
